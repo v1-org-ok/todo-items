0ddc248d07a442855b462b7d338deeb038640623
Visrut <visrut@incubyte.co>
2024-06-20 17:40:38 +0530

    Add Test for app/controllers/todos_controller.rb

--- spec/controllers/application_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ApplicationController, type: :controller do
-  # Add your tests here
-end
--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class TodosControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get todos_url
+    assert_response :success
+    assert_not_nil assigns(:todos)
+  end
+
+  test "should create todo" do
+    assert_difference('Todo.count') do
+      post todos_url, params: { todo: { description: 'Test todo' } }
+    end
+    assert_redirected_to todos_url
+  end
+
+  test "should destroy todo" do
+    todo = Todo.create(description: 'Test todo')
+    assert_difference('Todo.count', -1) do
+      delete todo_url(todo)
+    end
+    assert_redirected_to todos_url
+  end
 end
