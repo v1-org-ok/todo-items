d612f997328a77dc326f805dc3e4979fbac500d1
Visrut <visrut@incubyte.co>
2024-06-21 14:36:20 +0530

    Add Test for app/controllers/todos_controller.rb

---
+++ spec/controllers/todos_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe TodosController, type: :controller do
+  describe 'GET #index' do
+    it 'returns a success response' do
+      get :index
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid params' do
+      it 'creates a new Todo' do
+        expect {
+          post :create, params: { todo: { description: 'Test Todo' } }
+        }.to change(Todo, :count).by(1)
+      end
+
+      it 'redirects to the todos list' do
+        post :create, params: { todo: { description: 'Test Todo' } }
+        expect(response).to redirect_to(todos_path)
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'destroys the requested todo' do
+      todo = Todo.create!(description: 'Test Todo')
+      expect {
+        delete :destroy, params: { id: todo.id }
+      }.to change(Todo, :count).by(-1)
+    end
+
+    it 'redirects to the todos list' do
+      todo = Todo.create!(description: 'Test Todo')
+      delete :destroy, params: { id: todo.id }
+      expect(response).to redirect_to(todos_path)
+    end
+  end
+end
